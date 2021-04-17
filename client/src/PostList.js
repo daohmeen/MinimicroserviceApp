0369724c89642c68dae4e39ec7d7f0030becb44c
Daohmeen <daoh.alameen@gmail.com>
2021-04-17 23:33:20 +0700

    add infra for kubernetes and fixed bux to use ingress

--- client/src/PostList.js
+++ client/src/PostList.js
@@ -1,33 +1,40 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import CommentCreate from './CommentCreate';
 import CommentList from './CommentList';
 
-// eslint-disable-next-line 
 export default () => {
-    const [posts,setPosts] = useState({});
+  const [posts, setPosts] = useState({});
 
-    const fetchPosts = async () => {
-        const res = await axios.get('http://localhost:4002/posts');
-        console.log(res.data)
-        setPosts(res.data);
-    };
+  const fetchPosts = async () => {
+    const res = await axios.get('http://posts.com/posts');
 
-    useEffect(() => {
-        fetchPosts();
-    },[]);
+    setPosts(res.data);
+  };
 
-    const renderPosts = Object.values(posts).map(post => {
-        return <div className="card" style={{width:'30%',marginBottom:'20px'}} key={post.id}>
-            <div className="card-body">
-                <h3>{post.title}</h3>
-                <CommentList comments={post.comments}/>
-                <CommentCreate postId={post.id}/>
-            </div>
-        </div>;
-    });
+  useEffect(() => {
+    fetchPosts();
+  }, []);
 
-    return <div className="d-flex flex-row flex-wrap justify-content-between">
-        {renderPosts}
-    </div>;
-}
+  const renderedPosts = Object.values(posts).map(post => {
+    return (
+      <div
+        className="card"
+        style={{ width: '30%', marginBottom: '20px' }}
+        key={post.id}
+      >
+        <div className="card-body">
+          <h3>{post.title}</h3>
+          <CommentList comments={post.comments} />
+          <CommentCreate postId={post.id} />
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <div className="d-flex flex-row flex-wrap justify-content-between">
+      {renderedPosts}
+    </div>
+  );
+};
